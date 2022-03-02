b878c296cbb4b7ea6c71150f362d6e591e4347f8
hioe <hIN@gmail.com>
2022-03-02 16:03:07 +0100

    plot that sun
    sun.jl can plot one day and has been changed to be able to change
    percision. it doesn't matter when you only plot one day though.
    
    sun2.jl is a mess of code that will be made to calculate kwh over a
    longer time

--- src/sun2.jl
+++ src/sun2.jl
@@ -1,7 +1,7 @@
 using Plots
 
-#dag = 300
 dag = 160
+#dag = 160
 
 eff = 0.15
 area = 30
@@ -25,18 +25,39 @@ I(n, t) = 1.1 * I_0 * 0.7^((1 / (abs(sin(alfa_s(n, t)))+sin(alfa_s(n, t)))/2)^0.
 I_p(n, t) = I(n, t) * (cos(teta_p - teta_s(n, t)) * cos(alfa_p - alfa_s(n, t)) + sin(alfa_s(n, t)) * sin(alfa_p) * (1 - cos(teta_p - teta_s(n, t))))
 
 
-P(x) = (*(((abs(I_p(dag, x))+I_p(dag, x))/2), eff, area))
+P(n, t) = (*(((abs(I_p(n, t))+I_p(n, t))/2), eff, area))
 
 
-x = 5:0.01:18
+s_date = 1
+e_date = 31
+s_time = 0
+e_time = 24
+step = 0.1
+
+kwh = 0
+
+for i in s_date:e_date
+    for j in s_time:step:e_time
+        #NEED good way to get number of light hours in a day or month. could count it but seems bumb
+        global kwh += P(i, j)
+    end
+end
+
+kwh /= (e_date-s_date)*((e_time-s_date)/step)
+
+print(kwh)
+
+P(x) = (*(((abs(I_p(floor(x/24), x))+I_p(floor(x/24), x))/2), eff, area))
+
+x = 24*dag+5:0.01:24*(dag+30)+20
 
 x = filter(x->x%12!=0,x)
 
 plot(x, P.(x), title = "Stockholm", label = "Juni")
 
-dag = 5
-plot!(P, 5, 18, label = "Januari")
-
+#P(x) = (*(((abs(I_p(dag, x))+I_p(dag, x))/2), eff, area))
+#plot!(P, 5, 18, label = "Januari")
+#plot!(test, 0, 24, label = "I/1000")
 
 xlabel!("tid[h]")
 ylabel!("P[W]")
